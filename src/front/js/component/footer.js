63c4e7ed21007b79dc955277e59d51221eed3f5e
Fiorella-vq <fio.89@hotmail.com>
2024-11-29 22:51:52 +0000

    Modificaciones en la vista del menu

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,34 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer
-		className="footer mt-auto py-3 text-center"
-		style={{
-			"fontFamily": "Mulish, sans-serif",
-			"width": "100%",
-			"position": "relative",
-
-		}}
-	>
-		<div className="card m-3 border border-warning">
-			<div className="card-body">
-				<h5 className="card-title">
-					¡Nos encantaría leer tu opinión! <i className="fa-regular fa-face-smile"></i>
-				</h5>
-				<div className="mb-3">
-					<textarea
-						className="form-control"
-						rows="3"
-						placeholder="Escribe aquí..."
-					></textarea>
-					<div className="d-flex justify-content-end">
-						<button type="button" className="btn btn-warning mt-3">
-							Enviar
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-		<p>Derechos reservados</p>
+	<footer className="footer mt-auto py-3 text-center">
+		<p>
+		Derechos reservados
+		</p>
 	</footer>
 );
