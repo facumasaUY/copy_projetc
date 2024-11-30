0c890d72e7b4a9ff8290c766f98345883509c54f
facumasaUY <facundomasa@gmail.com>
2024-11-30 04:11:13 +0000

    Update 2

--- src/front/js/pages/shoppingCart.js
+++ src/front/js/pages/shoppingCart.js
@@ -1,13 +1,31 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import "../../styles/shoppingCart.css";
 
 export const ShoppingCart = () => {
 	const { store, actions } = useContext(Context);
    return (
-    <div  className="" style={{color: "#3865e5", backgroundColor:"white"}}>
+    <div  className="shoppingcart">
         <h1>Lista de Compras</h1>
         <div className="listCart">
-
+            <div className="foodImage">
+                <img src="https://images.mrcook.app/recipe-image/018f9b27-06a0-7deb-bc5d-b4ca97d2beb0"/>
+            </div>
+            <div className="foodName">
+                NAME
+            </div>
+            <div className="precioTotal">
+                $250
+            </div>
+            <div className="cantidad">
+                <span className="menos">
+                {"<"}
+                </span>
+                <span>1</span>
+                <span className="mas">
+                    {">"}
+                    </span>
+            </div>
         </div>
         <div className="btn" >
             <button className="close">CERRAR</button>
