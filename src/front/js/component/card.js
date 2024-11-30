2870201f1e0f9a79747a84c44aae614cb4935645
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-11-30 19:04:22 +0000

    cambios en card

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"; 
 
-export const Feedback = ({ initialRating = 0 }) => { 
+export const Feedback = ({ initialRating = 0 , id,name,imageURL,description}) => { 
 
   const [rating, setRating] = useState(initialRating); 
 
@@ -14,9 +14,9 @@ export const Feedback = ({ initialRating = 0 }) => {
     starElements.push(
       <span key={i} onClick={() => handleClickStar(i)}>
         {filled ? ( 
-          <i className="fas fa-star " style={{ color:"blue"}}></i>
+          <i className="fas fa-star " style={{ color:"gold"}}></i>
         ) : (
-          <i className="far fa-star" style={{ color:"blue"}}></i>
+          <i className="far fa-star" style={{ color:"gold"}}></i>
         )}
       </span>
     );
@@ -26,16 +26,16 @@ export const Feedback = ({ initialRating = 0 }) => {
     <div className="container d-flex space-between m-3 justify-content-center">
       <div className="card " style={{ width: "18rem" , border: "2px solid blue" }}>
         <img
-          src="https://jumboalacarta.com.ar/wp-content/uploads/2019/06/shutterstock_521741356-1024x684.jpg"
+          src="https://cdn0.recetasgratis.net/es/posts/4/3/6/arroz_con_pollo_al_curry_28634_orig.jpg"
+          // {imageURL + id} 
           className="card-img-top"
-          alt="Plato de comida"
+          alt={name}
         />
         <div className="card-body border-primary">
-          <h3 className="card-title text-primary">Ingredientes</h3>
-		  <p>Pollo</p>
-		  <p>Lechuga</p>
-		  <p>Tomate</p>
-		  <p>Palta</p>
+          <h3 className="card-title text-primary">Descripcion
+            {description}</h3>
+		  <p>Plato con enslada de quinoa ,verduras y pollo .</p>
+		  
           <p className="card-text">
             {starElements} {}
           </p>
