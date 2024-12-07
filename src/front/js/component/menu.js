eaa36ffd7f3336258bdb2c85a8fae011b6602cc4
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2024-12-07 16:42:21 +0000

    :recycle: Refactor Menu component to enhance navbar structure and improve shopping cart functionality

--- src/front/js/component/menu.js
+++ src/front/js/component/menu.js
@@ -154,127 +154,132 @@ export const Menu = () => {
     navigate("/payment");
   };
 
+  const MenuNavbar = (props) => {
+    return (
+      <nav className="navbar bg-body-tertiary">
 
-  return (<div className="container mt-3">
-
-    <nav className="navbar bg-body-tertiary">
-
-      <div className="container-fluid d-flex justify-content-between align-items-center" >
-        <div className="dropdown">
-          <a className="navbar-brand d-flex align-items-center dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src={andalogofood} alt="Anda Food Logo" style={{ width: "50px", height: "50px", marginRight: "10px", "borderRadius": "10px" }} />
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Mi cuenta</a></li>
-            <li><a className="dropdown-item" href="#">Mis compras</a></li>
-            <li><a className="dropdown-item" href="#">Cerrar Sesión</a></li>
-          </ul>
-        </div>
-
-
-        {/* El carrito de compras */}
-
-        <div className="shoppingCart d-flex">
-
-          {/* Boton de Carrito */}
-
-          <div className="flex-direction-column">
-            <Link to={"/form/:theid"}><p><a className="link-opacity-10-hover m-1  " href="#">Déjanos tu comentario</a></p></Link>
-
-            <button className="btn m-1 " type="button" style={{ backgroundColor: "rgb(56, 101, 229)", "color": "white" }}
-              onClick={() => irAReservaDeLugar()}
-            >
-              Reserva de lugar
-            </button>
-            <button className="btn m-1 " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" style={{ "backgroundColor": "rgb(56, 101, 229)", "color": "white" }}>
-              <i className="fa-solid fa-cart-shopping"></i>
-            </button>
+        <div className="container-fluid d-flex justify-content-between align-items-center" >
+          <div className="dropdown">
+            <a className="navbar-brand d-flex align-items-center dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src={andalogofood} alt="Anda Food Logo" style={{ width: "50px", height: "50px", marginRight: "10px", "borderRadius": "10px" }} />
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Mi cuenta</a></li>
+              <li><a className="dropdown-item" href="#">Mis compras</a></li>
+              <li><a className="dropdown-item" href="#">Cerrar Sesión</a></li>
+            </ul>
           </div>
 
-          {/* Ventana */}
-          <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
 
-            {/* Título */}
-            <div className="offcanvas-header" style={{
-              color: "white",
-              backgroundColor: "#3865e5"
-            }}>
-              <h1>Carrito</h1>
+          {/* El carrito de compras */}
+
+          <div className="shoppingCart d-flex">
+
+            {/* Boton de Carrito */}
+
+            <div className="flex-direction-column">
+              <Link to={"/form/:theid"}><p><a className="link-opacity-10-hover m-1  " href="#">Déjanos tu comentario</a></p></Link>
+
+              <button className="btn m-1 " type="button" style={{ backgroundColor: "rgb(56, 101, 229)", "color": "white" }}
+                onClick={() => irAReservaDeLugar()}
+              >
+                Reserva de lugar
+              </button>
+              <button className="btn m-1 " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" style={{ "backgroundColor": "rgb(56, 101, 229)", "color": "white" }}>
+                <i className="fa-solid fa-cart-shopping"></i>
+              </button>
             </div>
 
-            {/* Body */}
-            <div className="listCart offcanvas-body position-relative" style={{ backgroundColor: "rgb(56, 101, 229, 0.3)" }}>
+            {/* Ventana */}
+            <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
 
-              {spinner && (<div className="spinner-grow d-flex justify-content-center" role="status" style={{ backgroundColor: "#3865e5" }}>
-                <span className="visually-hidden">Loading...</span>
-              </div>)}
+              {/* Título */}
+              <div className="offcanvas-header" style={{
+                color: "white",
+                backgroundColor: "#3865e5"
+              }}>
+                <h1>Carrito</h1>
+              </div>
 
-              {listCart.length === 0 ? (
-                <p>El carrito está vacío.</p>
-              ) : (
-                listCart.map((item, index) => (
+              {/* Body */}
+              <div className="listCart offcanvas-body position-relative" style={{ backgroundColor: "rgb(56, 101, 229, 0.3)" }}>
+
+                {props.spinner && (<div className="spinner-grow d-flex justify-content-center" role="status" style={{ backgroundColor: "#3865e5" }}>
+                  <span className="visually-hidden">Loading...</span>
+                </div>)}
+
+                {listCart.length === 0 ? (
+                  <p>El carrito está vacío.</p>
+                ) : (
+                  listCart.map((item, index) => (
 
 
-                  <div key={index} className="d-flex align-items justify-content-evenly" style={{ backgroundColor: "white" }}>
+                    <div key={index} className="d-flex align-items justify-content-evenly" style={{ backgroundColor: "white" }}>
 
-                    <div className="foodImage d-flex justify-content-center align-items-center">
-                      <img src={item.img} />
-                    </div>
+                      <div className="foodImage d-flex justify-content-center align-items-center">
+                        <img src={item.img} />
+                      </div>
 
-                    <div className="foodName d-flex justify-content-center align-items-center" style={{ color: "#3865e5" }}>
-                      {item.name}
-                    </div>
+                      <div className="foodName d-flex justify-content-center align-items-center" style={{ color: "#3865e5" }}>
+                        {item.name}
+                      </div>
 
-                    <div className="precioTotal d-flex justify-content-center align-items-center" style={{ color: "#3865e5" }}>
-                      {item.price}
-                    </div>
+                      <div className="precioTotal d-flex justify-content-center align-items-center" style={{ color: "#3865e5" }}>
+                        {item.price}
+                      </div>
 
-                    <div className="cantidad d-flex justify-content-center align-items-center">
-                      <span className="menos" style={{
-                        width: "30px",
-                        height: "30px",
-                        color: "white",
-                        backgroundColor: "#3865e5",
-                        borderRadius: "50%",
-                        cursor: "pointer",
-                        margin: "0 10px",
-                        textAlign: "center"
-                      }}>
-                        {"<"}
-                      </span>
-                      <span>1</span>
-                      <span className="mas" style={{
-                        width: "30px",
-                        height: "30px",
-                        color: "white",
-                        backgroundColor: "#3865e5",
-                        borderRadius: "50%",
-                        cursor: "pointer",
-                        margin: "0 10px",
-                        textAlign: "center"
-                      }}>
-                        {">"}
-                      </span>
-                    </div>
+                      <div className="cantidad d-flex justify-content-center align-items-center">
+                        <span className="menos" style={{
+                          width: "30px",
+                          height: "30px",
+                          color: "white",
+                          backgroundColor: "#3865e5",
+                          borderRadius: "50%",
+                          cursor: "pointer",
+                          margin: "0 10px",
+                          textAlign: "center"
+                        }}>
+                          {"<"}
+                        </span>
+                        <span>1</span>
+                        <span className="mas" style={{
+                          width: "30px",
+                          height: "30px",
+                          color: "white",
+                          backgroundColor: "#3865e5",
+                          borderRadius: "50%",
+                          cursor: "pointer",
+                          margin: "0 10px",
+                          textAlign: "center"
+                        }}>
+                          {">"}
+                        </span>
+                      </div>
 
-                  </div>)))}
+                    </div>)))}
 
-              <div className="btn position-absolute bottom-0 start-0 end-0 d-flex justify-content-between" >
-                <button type="button" className="close align-self-start" data-bs-dismiss="offcanvas" aria-label="Close">VOLVER</button>
-                <button className="pay align-self-end" id="process-checkout" onClick={() => handleCompra()}>IR A PAGAR</button>
+                <div className="btn position-absolute bottom-0 start-0 end-0 d-flex justify-content-between" >
+                  <button type="button" className="close align-self-start" data-bs-dismiss="offcanvas" aria-label="Close">VOLVER</button>
+                  <button className="pay align-self-end" id="process-checkout" onClick={() => handleCompra()}>IR A PAGAR</button>
+                </div>
+
+
               </div>
 
 
+
             </div>
 
-
-
           </div>
-
         </div>
-      </div>
-    </nav>
+      </nav>
+    )
+  };
 
+
+  return (<div className="container mt-3">
+
+    <MenuNavbar spinner={spinner}/>
 
     {/* Menu del dia */}
     <div className="menudeldia2 mt-3" style={{ marginBottom: "20px", fontFamily: "Mulish, sans-serif" }}>
